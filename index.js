5f5023f6bba5a1f36808b1ac16f9ffd6fa4f50d8
Daniel Sisovic <d.sisovic777@gmail.com>
2023-12-18 21:45:29 +0100

    Finish

--- index.js
+++ index.js
@@ -8,60 +8,18 @@
         { category: 'obese', range: [30, 1000] }
     ];
 
-    const handleSwitchLayout = () => {
-        const metricContainer = document.querySelector('#metric');
-        const imperialContainer = document.querySelector('#imperial');
-        const radioButtons = document.querySelectorAll('[name=unit]');
+    const clearInputFields = () => {
+        const numberInputFields = document.querySelectorAll('input[type="text"]');
 
-        radioButtons.forEach(radioButton => {
-            radioButton.addEventListener('change', () => {
-                clearBmiResult();
-
-                if (radioButton.value === 'Imperial') {
-                    metricContainer.style.display = 'none';
-                    imperialContainer.style.display = 'flex';
-                    return;
-                }
-
-                metricContainer.style.display = 'flex';
-                imperialContainer.style.display = 'none';
-            });
-        });
+        numberInputFields.forEach(input => input.value = '');
     };
 
-    const handleMetricBmi = () => {
-        const heightElement = document.querySelector('#height');
-        const weightElement = document.querySelector('#weight');
-
-        [heightElement, weightElement].forEach(element => {
-            element.addEventListener('input', () => calculateMetricBmiValue(heightElement.value, weightElement.value));
-        })
-    };
-
-    const handleImperialBmi = () => {
-        const heightFtElement = document.querySelector('#height-ft');
-        const heightInElement = document.querySelector('#height-in');
-
-        const weightStElement = document.querySelector('#weight-st');
-        const weightLbsElement = document.querySelector('#weight-lbs');
-
-        [heightFtElement, heightInElement, weightStElement, weightLbsElement].forEach(element => {
-            element.addEventListener('input', () => calculateImeprialBmiValue(
-                heightFtElement.value, heightInElement.value, 
-                weightStElement.value, weightLbsElement.value
-            ));
-        })
-    };
-
-    const clearBmiResult = () => {
+    const resetBmiResult = () => {
         const bmiElement = document.querySelector('#bmi');
         const bmiTextElement = document.querySelector('#bmi-text');
         const bmiResultElement = document.querySelector('#bmi-result');
         const bmiWelcomeElement = document.querySelector('#bmi-welcome');
         const cardFooterElement = document.querySelector('.card__footer');
-        const numberInputFields = document.querySelectorAll('input[type="number"]');
-
-        numberInputFields.forEach(input => input.value = '');
 
         bmiResultElement.style.gap = '0';
         bmiElement.style.display = 'none';
@@ -77,21 +35,66 @@
         const bmiWelcomeElement = document.querySelector('#bmi-welcome');
         const cardFooterElement = document.querySelector('.card__footer');
 
-        bmiElement.textContent = bmi;
         bmiResultElement.style.gap = '0.8rem';
         cardFooterElement.style.gap = '2.4rem';
+
+        bmiElement.textContent = bmi;
         bmiElement.style.display = 'inline-block';
         bmiWelcomeElement.textContent = 'Your BMI is...';
         bmiTextElement.innerHTML = getBmiTextDescription(Number.parseFloat(bmi), height);
     };
 
-    const calculateImeprialBmiValue = (heightFt, heightIn, weightSt, weightLbs) => {
-        const allValuesValid = [
-            numberRegexp.test(heightFt), numberRegexp.test(heightIn), 
-            numberRegexp.test(weightSt), numberRegexp.test(weightLbs)
-        ];
+    const handleSwitchLayout = () => {
+        const metricContainer = document.querySelector('#metric');
+        const imperialContainer = document.querySelector('#imperial');
+        const radioButtons = document.querySelectorAll('[name=unit]');
 
-        if (!allValuesValid.every(isValid => isValid)) { return; }
+        radioButtons.forEach(radioButton => {
+            radioButton.addEventListener('change', () => {
+                clearInputFields();
+                resetBmiResult();
+
+                if (radioButton.value === 'Imperial') {
+                    metricContainer.style.display = 'none';
+                    imperialContainer.style.display = 'flex';
+                    return;
+                }
+
+                metricContainer.style.display = 'flex';
+                imperialContainer.style.display = 'none';
+            });
+        });
+    };
+
+    const attachInputListeners = (elements, callback) => {
+        elements.forEach(element => element.addEventListener('input', callback));
+    };
+
+    const handleMetricBmi = () => {
+        const heightElement = document.querySelector('#height');
+        const weightElement = document.querySelector('#weight');
+
+        attachInputListeners([heightElement, weightElement], () => calculateMetricBmiValue(heightElement.value, weightElement.value));
+    };
+
+    const handleImperialBmi = () => {
+        const heightFtElement = document.querySelector('#height-ft');
+        const heightInElement = document.querySelector('#height-in');
+        const weightStElement = document.querySelector('#weight-st');
+        const weightLbsElement = document.querySelector('#weight-lbs');
+
+        const elements = [heightFtElement, heightInElement, weightStElement, weightLbsElement];
+        
+        attachInputListeners(elements, () => calculateImperialBmiValue(
+            heightFtElement.value, heightInElement.value,
+            weightStElement.value, weightLbsElement.value
+        ));
+    };
+
+    const calculateImperialBmiValue = (heightFt, heightIn, weightSt, weightLbs) => {
+        const allValuesValid = [heightFt, heightIn, weightSt, weightLbs].every(value => numberRegexp.test(value));
+
+        if (!allValuesValid) return;
 
         const height = getImperialHeightInMeters(heightFt, heightIn);
         const bmi = getImperialBmi(heightFt, heightIn, weightSt, weightLbs);
@@ -99,19 +102,17 @@
         setBmiText(bmi, height * 100);
     };
 
-    const getImperialHeightInMeters = (heightFeet, heightInches) => {
-        return heightFeet * 0.3048 + heightInches * 0.0254;
-    };
+    const getImperialHeightInMeters = (heightFeet, heightInches) => heightFeet * 0.3048 + heightInches * 0.0254;
 
     const getImperialBmi = (heightFt, heightIn, weightSt, weightLbs) => {
         const heightMeters = getImperialHeightInMeters(heightFt, heightIn);
         const weightKg = weightSt * 6.35029 + weightLbs * 0.453592;
-    
+
         return (weightKg / (heightMeters * heightMeters)).toFixed(1);
-    }
+    };
 
     const calculateMetricBmiValue = (height, weight) => {
-        if (!numberRegexp.test(height) || !numberRegexp.test(weight)) { return; }
+        if (!numberRegexp.test(height) || !numberRegexp.test(weight)) return;
 
         const heightInMeters = height / 100;
         const bmi = (weight / (heightInMeters * heightInMeters)).toFixed(1);
@@ -125,16 +126,15 @@
         const weightUpper = 24.9 * heightInMeters * heightInMeters;
 
         return [weightLower.toFixed(1), weightUpper.toFixed(1)];
-    }
+    };
 
     const getBmiTextDescription = (bmi, height) => {
         const matchingCategory = bmiCategories.find(item => {
             const [min, max] = item.range;
-
             return bmi >= min && bmi <= max;
         });
 
-        if (!matchingCategory) { return 'Invalid BMI'; }
+        if (!matchingCategory) return 'Invalid BMI';
 
         const { category } = matchingCategory;
         const [lower, upper] = calculateWeightRange(height);
